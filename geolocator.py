cbcb12173dfeea5bfd582601ef73abd53b9d1958
Alex <alexvkleschov@gmail.com>
2022-07-10 20:12:08 +0400

    add Coordinate pydantic dataclass

--- geolocator.py
+++ geolocator.py
@@ -1,18 +1,19 @@
 from geopy.geocoders import Nominatim
+from pydantic import BaseModel
 
 
-def get_coordinates(query: str) -> dict:
+class Coordinate(BaseModel):
+    latitude: float
+    longitude: float
+
+
+def get_coordinates(query: str) -> Coordinate:
     """Function use string query to achieve address and geo coordinates of place"""
     locator = Nominatim(user_agent="GetLoc")
 
     # entering the location name
     get_loc = locator.geocode(query)
 
-    # TODO: Add dataclass
-    location = {
-        'address': get_loc.address,
-        'lat': get_loc.latitude,
-        'lon': get_loc.longitude,
-    }
+    location = Coordinate(latitude=get_loc.latitude, longitude=get_loc.longitude)
 
     return location
--- weatherapi.py
+++ weatherapi.py
@@ -2,8 +2,9 @@ import os
 import pathlib
 
 import requests
-
 from dotenv import load_dotenv
+
+from geolocator import Coordinate
 
 load_dotenv(os.path.join(pathlib.Path(__file__).parent.absolute(), '.env'))
 
@@ -11,11 +12,11 @@ API_KEY = os.getenv('API_KEY')
 url = 'https://api.openweathermap.org/data/2.5/weather'
 
 
-def get_weather(location):
+def get_weather(location: Coordinate):
     # Request response from OpenWeather API by geolocation
     params = {
-        'lat': location['lat'],
-        'lon': location['lon'],
+        'lat': location.latitude,
+        'lon': location.longitude,
         'appid': API_KEY,
     }
     response = requests.get(url, params=params)
