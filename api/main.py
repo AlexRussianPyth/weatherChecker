238b7c677d12e620c08dcc72153f2f713185d9fc
Alex <alexvkleschov@gmail.com>
2022-07-17 16:41:13 +0400

    testing pre commit

--- api/main.py
+++ api/main.py
@@ -8,30 +8,28 @@ from weather.weather import get_forecast
 
 app = FastAPI(
     title=config.PROJECT_NAME,
-    docs_url='/api/openapi',
-    openapi_url='/api/openapi.json',
+    docs_url="/api/openapi",
+    openapi_url="/api/openapi.json",
 )
 
+love = "sdgsgs"
 
-@app.get('/')
+
+@app.get("/")
 async def index():
-    return {'tex222t3': 'tes3t'}
+    return {"tex222t3": "tes3t"}
 
 
-@app.get('/weathers/forecast')
+@app.get("/weathers/forecast")
 async def weather_forecast():
     forecasts = get_forecast()
     dict_forecasts = [obj.dict() for obj in forecasts]
-    return {
-        'forecasts': dict_forecasts
-    }
+    return {"forecasts": dict_forecasts}
 
 
-@app.get('/weathers/{day}')
+@app.get("/weathers/{day}")
 async def weather_for_day(day: str):
-    return {
-        'specific_weather': day
-    }
+    return {"specific_weather": day}
 
 
 # TODO Make function to return weather by day of week (use enum)
@@ -40,10 +38,10 @@ async def weather_by_weekday(weekday: Weekday):
 
     match weekday:
         case Weekday.MONDAY:
-            return {"message": 'You try monday weather'}
+            return {"message": "You try monday weather"}
         case Weekday.TUESDAY:
-            return {"message": 'You try tuesday weather'}
+            return {"message": "You try tuesday weather"}
 
 
 def api_run():
-    uvicorn.run('api.main:app', reload=config.UVICORN_RELOAD, host='0.0.0.0', port=8000)
+    uvicorn.run("api.main:app", reload=config.UVICORN_RELOAD, host="0.0.0.0", port=8000)
